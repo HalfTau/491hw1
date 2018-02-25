e6c410b1dbccb8b25401a479801613f92e9f2df5
HalfTau <bryansands@student.olympic.edu>
2018-02-25 13:50:22 -0800

    copying files, implemented "map"

---
+++ main.js
@@ -0,0 +1,79 @@
+var AM = new AssetManager();
+
+function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop) {
+    this.spriteSheet = spriteSheet;
+    this.startX = startX * frameWidth;
+    this.startY = startY * frameHeight;
+    this.frameWidth = frameWidth;
+    this.sheetWidth = sheetWidth;
+    this.frameDuration = frameDuration;
+    this.frameHeight = frameHeight;
+    this.frames = frames;
+    this.totalTime = frameDuration * frames;
+    this.elapsedTime = 0;
+    this.loop = loop;
+}
+
+Animation.prototype.drawFrame = function (tick, ctx, x, y) {
+    this.elapsedTime += tick;
+    if (this.isDone()) {
+        if (this.loop) this.elapsedTime = 0;
+    }
+    var frame = this.currentFrame();
+    var xindex = 0;
+    var yindex = 0;
+    xindex = frame % this.sheetWidth;
+    yindex = Math.floor(frame / this.sheetWidth);
+
+    ctx.drawImage(this.spriteSheet,
+                 xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
+                 this.frameWidth, this.frameHeight,
+                 x, y,
+                 this.frameWidth * this.scale,
+                 this.frameHeight * this.scale);
+}
+
+Animation.prototype.currentFrame = function () {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+}
+
+Animation.prototype.isDone = function () {
+    return (this.elapsedTime >= this.totalTime);
+}
+
+// no inheritance
+function Background(game, spritesheet) {
+    this.x = 0;
+    this.y = 0;
+    this.spritesheet = spritesheet;
+    this.game = game;
+    this.ctx = game.ctx;
+};
+
+Background.prototype.draw = function () {
+    this.ctx.drawImage(this.spritesheet,
+                   this.x, this.y);
+};
+
+Background.prototype.update = function () {
+};
+
+var gameEngine = new GameEngine();
+var map = new Map(gameEngine);
+AM.queueDownload("./img/knight.png");
+AM.queueDownload("./img/tile1.png");
+AM.queueDownload("./img.tile2.png")
+
+AM.downloadAll(function () {
+    var canvas = document.getElementById("gameWorld");
+    var ctx = canvas.getContext("2d");
+
+
+    gameEngine.init(ctx);
+    gameEngine.start();
+
+
+    map.readMap(new MapData().map);
+
+    console.log("All Done!");
+});
---
+++ map.js
@@ -0,0 +1,55 @@
+function Map(gameEngine) {
+    this.game = gameEngine;
+    this.simpleMapData = [];
+    this.mapList = [];
+    // Note: not needed when you give mapData.
+
+}
+Map.prototype.constructor = Map;
+
+Map.prototype.readMap = function (mapData) {
+
+    for (i = 0; i < mapData.length; i++) {
+        this.mapList[i] = new Array(mapData.length);
+        for (j = 0; j < mapData[i].length; j++) {
+            x = j;
+            y = i;
+            tileType = mapData[i][j];
+            //console.log(twodtoisoX(x, y) + ' '+ twodtoisoY(x, y));
+            var tile = new Tile(this.game, tileType, x, y);
+            //this.game.addEntity(tile);
+            this.mapList[i][j] = tile;
+        }
+    }
+}
+
+// tiling going down
+function Tile(game, tileType, x, y) {
+    //this.animation = new Animation(ASSET_MANAGER.getAsset("./img/grass.png"), 0, 0, 58, 30, 1, .15, 1, true);
+    this.gfxString = '';
+    this.normalImage = "./img/tile1.png";
+    this.rockImage = "./img/tile2.png";
+
+    if (tileType === 0) {
+        this.gfxString = this.normalImage;
+    } else if (tileType === 1) {
+        this.gfxString = this.rockImage;
+    }
+    this.thing = null;
+    this.image = new Image();
+    this.image.src = this.gfxString;
+    this.game = game;
+    this.x = x;
+    this.y = y;
+    this.tileType = tileType;
+}
+Tile.prototype = Object.create(Entity.prototype);
+Tile.prototype.constructor = Tile;
+
+Tile.prototype.draw = function (ctx) {
+    ctx.drawImage(
+        this.image,
+          this.x * 56,
+          this.y * 56
+    );
+}
